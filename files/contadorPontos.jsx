c9e49b4ff14ff30d58a83f22f21999762e8eea68
Cesar Mauricio Chauchuty <cesar.chauchuty@escola.pr.gov.br>
2026-03-20 12:28:06 -0300

    Update contadorPontos.jsx

--- files/contadorPontos.jsx
+++ files/contadorPontos.jsx
@@ -1,57 +1,25 @@
 // Importações
-import { View, Text, Button, StyleSheet } from 'react-native';
 import { useState } from 'react';
+import { Text, Button, View, StyleSheet } from 'react-native';
 
 // Componente Principal
 export default function App() {
-  // Declarações
-  // Estados
-  const [pontosTeamA, setPontosTeamA] = useState(0);
+  // Variáveis, Constantes, Estado
+  const [numero, setNumero] = useState(0)
 
-  // Funções
-  function incrementar() {
-    setPontosTeamA(pontosTeamA + 1);
+  const incrementar = () => {
+    setNumero(numero + 1)
+  };
+
+  const decrementar = () => {
+    numero > 0 && setNumero(numero - 1)
   }
-
-  function decrementar() {
-    if (pontosTeamA > 0) {
-      setPontosTeamA(pontosTeamA - 1);
-    }
-  }
-
-  // Redenrização
+ 
   return (
-    // Container Principal
-    <View style={styles.container}>
-      <View style={styles.team}>
-        <Text>Equipe A</Text>
-        <Text>Pontos: {pontosTeamA}</Text>
-        <Button title="Incrementar" onPress={incrementar}></Button>
-        <Button title="Decrementar" onPress={decrementar}></Button>
-      </View>
-
-      <View style={styles.team}>
-        <Text>Equipe B</Text>
-        <Text>Pontos: 0</Text>
-        <Button title="Incrementar" color="red"></Button>
-        <Button title="Decrementar" color="red"></Button>
-      </View>
+    <View>
+      <Button title="Incrementar" onPress={incrementar} />
+      <Button title="Decrementar" onPress={decrementar} color="red"/>
+      <Text>{numero}</Text>
     </View>
   );
 }
-
-// Estilização
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    borderWidth: 6,
-    borderColor: 'red',
-  },
-  team: {
-    flex: 1,
-    borderWidth: 6,
-    borderColor: 'blue',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-});
